66e257e3746b55540c707617ca18a16f414921c9
Kevin Jordan Galarza Vera <kevin.galarza@sofka.local>
2024-12-12 10:40:31 -0500

    conexión a bd y respository

--- src/main/java/com/kgalarza/banco/repository/ClienteRepository.java
+++ src/main/java/com/kgalarza/banco/repository/ClienteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ClienteRepository implements JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
